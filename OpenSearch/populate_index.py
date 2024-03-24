200e39fbaba5fffdbe0d207d0a17f5dd1181bdea
Guilherme Fernandes <84087774+Gui28F@users.noreply.github.com>
2024-03-24 10:51:58 +0000

    cleaned up the code and added persistent storage for the embeddings

--- OpenSearch/populate_index.py
+++ OpenSearch/populate_index.py
@@ -1,86 +1,58 @@
 import pprint as pp
 import OpenSearch.transformer as tr
 import OpenSearch.opensearch as OpenSearchUtil
-import numpy as np
-from transformers import AutoTokenizer, AutoModel
-import torch
-import torch.nn.functional as F
 from sklearn.feature_extraction.text import CountVectorizer
+import os
+import pickle
 
 def embed_recipes(recipes, embedding_text):
-    recipe_emb = tr.encode(embedding_text)
+    recipe_emb_file = 'recipe_emb.pkl'
+    
+    # Check if recipe_emb_file exists
+    if os.path.exists(recipe_emb_file):
+        # If file exists, load recipe_emb from the file
+        with open(recipe_emb_file, 'rb') as f:
+            recipe_emb = pickle.load(f)
+    else:
+        # If file does not exist, compute recipe_emb and save it to the file
+        recipe_emb = tr.encode(embedding_text)
+        with open(recipe_emb_file, 'wb') as f:
+            pickle.dump(recipe_emb, f)
+
     for i,recipe in enumerate(recipes):
         recipe["sentence_embedding"] = recipe_emb[i].numpy()
         res = OpenSearchUtil.opensearch_end.add_recipe(i, recipe)
         pp.pprint(res)
-        pp.pprint(recipe)
        
-
+def vectorize(text):
+    vectorizer = CountVectorizer(ngram_range=(1,1),analyzer="word", stop_words='english')
+    vectorizer.fit_transform(text)
+    text = " ".join(vectorizer.get_feature_names_out())
+    return text
 
 def populate_index(data):
     recipes = []
     embedding_text = []
-    index = 0
-    while True:
-        try:
-            document_sample = {}
+
+    for index in range(len(data)):
+            recipe_sample = {}
             recipe_id = str(index)
-            document_sample["recipeName"] = data[recipe_id]["displayName"]
-            document_sample["prepTimeMinutes"] = data[recipe_id]["prepTimeMinutes"]
-            document_sample["cookTimeMinutes"] = data[recipe_id]["cookTimeMinutes"]
-            document_sample["totalTimeMinutes"] = data[recipe_id]["totalTimeMinutes"]
-            document_sample["difficultyLevel"] = data[recipe_id]["difficultyLevel"]
-            document_sample["images"] = [image["url"] for image in data[recipe_id]["images"]]
-            document_sample["videos"] =[{ "title": video["title"], "url": video["url"]} for video in data[recipe_id]["videos"]] #Returns array of video objects
-            document_sample["tools"] = [tool["displayName"] for tool in data[recipe_id]["tools"]]
-            document_sample["cuisines"] = data[recipe_id]["cuisines"]
-            document_sample["courses"] = data[recipe_id]["courses"]
-            document_sample["diets"] = data[recipe_id]["diets"]
-            document_sample["ingredients"] = [ing["displayText"] for ing in data[recipe_id]["ingredients"]]
-            recipes.append(document_sample)
+            recipe_sample["recipeName"] = data[recipe_id]["displayName"]
+            recipe_sample["prepTimeMinutes"] = data[recipe_id]["prepTimeMinutes"]
+            recipe_sample["cookTimeMinutes"] = data[recipe_id]["cookTimeMinutes"]
+            recipe_sample["totalTimeMinutes"] = data[recipe_id]["totalTimeMinutes"]
+            recipe_sample["difficultyLevel"] = data[recipe_id]["difficultyLevel"]
+            recipe_sample["images"] = [image["url"] for image in data[recipe_id]["images"]]
+            recipe_sample["videos"] =[{ "title": video["title"], "url": video["url"]} for video in data[recipe_id]["videos"]]
+            recipe_sample["tools"] = [tool["displayName"] for tool in data[recipe_id]["tools"]]
+            recipe_sample["cuisines"] = data[recipe_id]["cuisines"]
+            recipe_sample["courses"] = data[recipe_id]["courses"]
+            recipe_sample["diets"] = data[recipe_id]["diets"]
+            recipe_sample["ingredients"] = [ing["displayText"] for ing in data[recipe_id]["ingredients"]]
+            recipes.append(recipe_sample)
 
-            vectorizer = CountVectorizer(ngram_range=(1,1),analyzer="word", stop_words='english')
-            recipe_embeding_text = [document_sample["recipeName"] + " " + " ".join(document_sample["ingredients"])]
-            vectorizer.fit_transform(recipe_embeding_text)
-            recipe_embeding_text = " ".join(vectorizer.get_feature_names_out())
-       
-            embedding_text.append(recipe_embeding_text)
-
-            index += 1
-        except KeyError:
-            print("End of data")
-            embed_recipes(recipes, embedding_text)
-            pp.pprint(document_sample)
-            break
-            
-
-# document_sample = {
-#     "recipeName": '',
-#     "prepTimeMinutes": 0,
-#     "cookTimeMinutes": 0,
-#     "totalTimeMinutes": 0,
-#     "difficultyLevel": '',
-#     "images": '',
-#     "videos": '', #complex object with title and url
-#     "tools": '',
-#     "cuisines": '',
-#     "courses": '',
-#     "diets": '',
-#     "ingredients": '',
-#     "sentence_embeddings": '', #Array that embeds the word
-# }
-
-
-#  "properties":{
-#      "cuisines" : { #Array of cuisine types, "mexican", "Asian" etc..., [str]
-#      "type" : "text" 
-#      },
-#      "courses" : { #Array of course types, "main" "breakfast" "sides" etc..., [str]
-#          "type" : "text"
-#      },
-#      "diets" : { #Array of diet types, "vegan" etc... [str]
-#          "type" : "text"
-#      },
-#      "ingredients" : { #Array of ingredient names, didn't save quantity cos that can be loaded later, [str]
-#          "type" : "text", 
-#      },
+            recipe_text = [recipe_sample["recipeName"] + " " + " ".join(recipe_sample["ingredients"])]
+            recipe_text = vectorize(recipe_text)
+            embedding_text.append(recipe_text)
+   
+    embed_recipes(recipes, embedding_text)
