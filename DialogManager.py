0b6fcefa0de6d234b133e3e0321d166636ef3f07
Guilherme Fernandes <84087774+Gui28F@users.noreply.github.com>
2024-05-29 09:46:08 +0100

    update

--- DialogManager.py
+++ DialogManager.py
@@ -1,8 +1,13 @@
 from statemachine import StateMachine, State
 from enum import Enum
+import random
+from models.SlotFilling import SlotFiller
+from OpenSearch.query_manager import QueryManager
+
+
 class Acronym(Enum):
     GETCURIOSITIESINTENT = "gci"
-    GREETTINGINTENT = "gi"
+    GREETINGINTENT = "gi"
     SELECTINTENT = "si"
     SHOWSTEPSINTENT = "ssi"
     MOREOPTIONSINTENT = "moi"
@@ -31,11 +36,30 @@ class Acronym(Enum):
     SUBSTITUTIONINTENT = "subi"
     STOPINTENT = "stopI"
 
+greetings = [
+    "Hi there! How can I assist you?",
+    "Hello!",
+    "Hey!",
+    "Hi!",
+    "Howdy!",
+    "Greetings!",
+    "Hey there!",
+    "Hiya!",
+    "Hello there!",
+    "Good day!",
+    "What lovely weather we're having.",
+    "I love the chinese government. CCP™",
+    "Long live Xi-Jing-Ping!",
+    "How do you do fellow kids, Bing Chillin, Xazioio Bing Chillin",
+    "I know how to cook meth, just ask me how!",
+    "Giraffes are 30 times more likely to get hit by lightning than people."
+]
 
+greeting = False
 
 class DialogManager(StateMachine):
-
-    start = State(initial=True)
+    greetings = State(initial=True)
+    start = State()
     suggestion_state = State()
     identify_process_state = State()
     recipe_selected_state = State()
@@ -44,18 +68,20 @@ class DialogManager(StateMachine):
     next_step_state = State()
     previous_step_state = State()
 
+
+    gi = greetings.to(start) | start.to(start)
     sugi = start.to(suggestion_state) | suggestion_state.to(suggestion_state) | identify_process_state.to(suggestion_state) | recipe_selected_state.to(suggestion_state)
     ipi = start.to(identify_process_state) | identify_process_state.to(identify_process_state) | suggestion_state.to(identify_process_state) | recipe_selected_state.to(identify_process_state)
     si = suggestion_state.to(recipe_selected_state) | identify_process_state.to(recipe_selected_state)
     ssi = recipe_selected_state.to(recipe_selected_state) 
-    igi = recipe_selected_state.to(recipe_selected_state)
+    ici = recipe_selected_state.to(recipe_selected_state)
     startSI = recipe_selected_state.to(enter_recipe_state)
     tcti = enter_recipe_state.to(start) | current_step_state.to(start) | next_step_state.to(start) | previous_step_state.to(start)
     stopI = enter_recipe_state.to(start) | current_step_state.to(start) | next_step_state.to(start) | previous_step_state.to(start)
     rti = enter_recipe_state.to(enter_recipe_state)
     asi = enter_recipe_state.to(enter_recipe_state)
     nsi = enter_recipe_state.to(current_step_state) | current_step_state.to(next_step_state) | next_step_state.to(next_step_state) | previous_step_state.to(next_step_state)
-    gtsi = enter_recipe_state.to(current_step_state) | next_step_state.to(current_step_state) | previous_step_state.to(current_step_state)
+    gtsi = enter_recipe_state.to(current_step_state) | next_step_state.to(current_step_state) | previous_step_state.to(current_step_state) | current_step_state.to(current_step_state)
     subi = current_step_state.to(current_step_state) | next_step_state.to(next_step_state) | previous_step_state.to(previous_step_state)
     cti = current_step_state.to(start) | next_step_state.to(start) # TODO ver melhor
     psi = next_step_state.to(previous_step_state) | previous_step_state.to(previous_step_state)
@@ -64,14 +90,46 @@ class DialogManager(StateMachine):
 
     def __init__(self):
         super(DialogManager, self).__init__()
+        #self._activate_initial_state()
+        self.send(convert_intent("GreetingIntent"))
+        self.slot_filler = SlotFiller()
+        #self.query_manager = QueryManager()
+      
+
+    def on_enter_greetings(self):
+        idx = random.randint(0, len(greetings) - 1)
+        print(greetings[idx])
+
+    #def on_enter_
+    
+    def on_enter_start(self, event: str, source: State, target: State, message: str = ""):
+        print("\nEntered start from transition: {0}".format(event))
+        print(message)
+        #self.slot_filling.
+        
+    
+    def on_enter_suggestion_state(self, event: str, source: State, target: State, message: str = ""):
+        print("\nEntered suggestion_state from transition: {0}".format(event))
+        print(message)
+        # Now we need to process the message of the user, e.g "Give me a suggestion of a recipe with tomato"
+        #self.slot_filling.
+        res = self.slot_filler.get_sugi_prompt_information(message)
+        print(res)
+        #print(self.query_manager.query_by_ingredients(res['ingredients']))
+
+        
+
+        
+        
+        
+        
 
 def convert_intent(intent):
     return Acronym[intent.upper()].value
 
-
 d = DialogManager()
-d.send(convert_intent("SuggestionsIntent"))
-d.send(convert_intent("SuggestionsIntent"))
+d.send(convert_intent("SuggestionsIntent"), message="Suprise me with a recipe for dinner") 
+d.send(convert_intent("SuggestionsIntent"),  message="My daughter's birthday is coming up, do you have any ideas for something for her party?")
 d.send(convert_intent("SelectIntent"))
 d.send(convert_intent("StartStepsIntent"))
 d.send(convert_intent("ResumeTaskIntent"))
@@ -82,5 +140,5 @@ d.send(convert_intent("SubstitutionIntent"))
 d.send(convert_intent("PreviousStepIntent"))
 d.send(convert_intent("PreviousStepIntent"))
 d.send(convert_intent("GotoStepIntent"))
-d.send(convert_intent("StopIntent"))
+d.send(convert_intent("StopIntent"),message="I want to stop this")
 print(d.current_state)
--- models/SlotFilling.py
+++ models/SlotFilling.py
@@ -5,18 +5,27 @@ import torch as torch
 This class will be used to obtain the variables from the user inputs, depending on the situation we will try and obtain the user variables
 and return them
 """
-debug = False
+debug = True
 
 model_name = "deepset/roberta-base-squad2"
 SCORE_THRESHOLD = 0.001
 
-#region - Sentences
-GENERIC_QUESTION = "What are we making or cooking?"
-INGREDIENT_QUESTION = "What are the ingredients?"
-DURATION_QUESTION = "What is the cooking time?"
-SERVINGS_QUESTION = "How many servings?"
-STYLE_QUESTION = "What is the cooking or food style?"
-DIFFICULTY_QUESTION = "How easy, hard or difficult is it?"
+#Suggest me something for dinner
+
+#region - Sentences For Identify Process State
+IPI_GENERIC_QUESTION = "What are we making or cooking?"
+IPI_INGREDIENT_QUESTION = "What are the ingredients?"
+IPI_DURATION_QUESTION = "What is the cooking time?"
+IPI_SERVINGS_QUESTION = "How many servings?"
+IPI_STYLE_QUESTION = "What is the cooking or food style?"
+IPI_DIFFICULTY_QUESTION = "How easy, hard or difficult is it?"
+#endregion
+
+#region - Sentences for Suggestion State
+SUGI_GENERIC_QUESTION = "What do they want to make or cook?"
+SUGI_OCCASION_QUESTION = "What is the occasion or event?"
+SUGI_ING_QUESTION = "What ingredients do they want to use?"
+
 #endregion
 
 class SlotFiller():
@@ -44,65 +53,66 @@ class SlotFiller():
             if(debug):print("DEBUG: Answer to question: \'{0}\' with context \'{1}\'\n Returned NULL : \'{2}\' had too low score: {3}\r\n".format(question, context, answer['answer'],answer['score']))
             return "NULL"
     
+    #region - Questions for getting recipe information
     #Returns the generic information about what the user is making, if they say "I want to bake a cake" it will return cake
-    def get_generic_information(self, user_input : str):
+    def get_generic_information_ipi(self, user_input : str):
         
-        question = GENERIC_QUESTION
+        question = IPI_GENERIC_QUESTION
         
         answer = self.get_variables(question,user_input)
         return answer
     
     #Returns the part of the sentence that contains the ingredients, could be like "pepperoni and cheese"
-    def get_ingredient_variables(self, user_input : str):
+    def get_ingredient_variables_ipi(self, user_input : str):
         
-        question = INGREDIENT_QUESTION
+        question = IPI_INGREDIENT_QUESTION
         
         answer = self.get_variables(question,user_input)
         return answer
     
     #Returns the part of the sentence that contains the pretended duration of the sentence, could return 30, or "less than half an hour"
-    def get_duration_variables(self, user_input : str):
+    def get_duration_variables_ipi(self, user_input : str):
         
-        question = DURATION_QUESTION
+        question = IPI_DURATION_QUESTION
         
         answer = self.get_variables(question,user_input)
         return answer
     
     #Returns the part of the sentence that contains the number of servings, usually just returns the number
-    def get_servings_variables(self, user_input : str):
+    def get_servings_variables_ipi(self, user_input : str):
         
-        question = SERVINGS_QUESTION
+        question = IPI_SERVINGS_QUESTION
         
         answer = self.get_variables(question,user_input)
         return answer
     
     #Returns the part of the sentence that contains the cooking style for our recipe, usually returns just the cooking style 'Mediterranean'
-    def get_cooking_style_variables(self, user_input : str):
+    def get_cooking_style_variables_ipi(self, user_input : str):
         
-        question = STYLE_QUESTION
+        question = IPI_STYLE_QUESTION
         
         answer = self.get_variables(question,user_input)
         return answer
     
     #Returns the part of the sentence that contains the cooking difficulty, could return 'easy' or 'not too difficult'
-    def get_cooking_difficulty(self, user_input : str):
+    def get_cooking_difficulty_ipi(self, user_input : str):
         
-        question = DIFFICULTY_QUESTION
+        question = IPI_DIFFICULTY_QUESTION
         
         answer = self.get_variables(question,user_input)
         return answer
     
     #Gets the generic information from the user's request to make something
-    def get_recipe_prompt_information(self, user_input : str):
+    def get_ipi_prompt_information(self, user_input : str):
         
         #Ask each question to our model in order to obtain all the 
         questions = {
-            'generic' : self.get_generic_information,
-            'ingredients' : self.get_ingredient_variables,
-            'duration' : self.get_duration_variables,
-            'servings' : self.get_servings_variables,
-            'style' : self.get_cooking_style_variables,
-            'difficulty' : self.get_cooking_difficulty, 
+            'generic' : self.get_generic_information_ipi,
+            'ingredients' : self.get_ingredient_variables_ipi,
+            'duration' : self.get_duration_variables_ipi,
+            'servings' : self.get_servings_variables_ipi,
+            'style' : self.get_cooking_style_variables_ipi,
+            'difficulty' : self.get_cooking_difficulty_ipi, 
         }
         
         information = {
@@ -119,7 +129,52 @@ class SlotFiller():
         
         if(debug):print("From input: {0}, Obtained:\n{1}".format(user_input,information))
         return information
+    #endregion
     
+    #region - Questions for getting suggestion information
+    
+    def get_generic_sugi(self, user_input : str):
+        
+        question = SUGI_GENERIC_QUESTION
+        
+        answer = self.get_variables(question,user_input)
+        return answer
+    
+    def get_occasion_sugi(self, user_input : str):
+        
+        question = SUGI_OCCASION_QUESTION
+        
+        answer = self.get_variables(question,user_input)
+        return answer
+    
+    def get_ing_sugi(self, user_input : str):
+        question = SUGI_ING_QUESTION
+        
+        answer = self.get_variables(question,user_input)
+        return answer
+    
+    def get_sugi_prompt_information(self, user_input : str):
+        
+        #Ask each question to our model in order to obtain all the 
+        questions = {
+            'generic' : self.get_generic_sugi,
+            'occasion' : self.get_occasion_sugi,
+            'ingredients' : self.get_ing_sugi
+        }
+        
+        information = {
+            'generic' : "",
+            'occasion' : "",
+            'ingredients' : ""
+
+        }
+        
+        for type,function in questions.items():
+            information[type] = function(user_input)
+        
+        if(debug):print("From input: {0}, Obtained:\n{1}".format(user_input,information))
+        return information
+    #endregion 
     
     #Debug function, to test a pair of 'question' : 'input' values
     def model_test(self, question : str, user_input : str):
@@ -129,8 +184,8 @@ class SlotFiller():
 ###################################################### TESTING ########################################################################
 #SlotFiller().get_ingredients("I would like a pizza with cheese and maybe a little tiny miniscule bit of pepperoni on it")
 
-sentence1 = "I would like to make a meatloaf with a cheese, ham, hazelnuts and paprika filling, it should feed 4 people and take less than 30 minutes. It should be done in a Mediterranean style, and of course, it should be easy to make as I'm not very good at cooking"
-sentence2 = "I would like to make a dessert for my daughter's birthday"
-sentence3 = "I would like to make burritos with cream cheese filling and salsa, it should feed my whole family" 
-manipulation = "How Many Servings? Carrots"
-SlotFiller().get_recipe_prompt_information(manipulation)
+# sentence1 = "I would like to make a meatloaf with a cheese, ham, hazelnuts and paprika filling, it should feed 4 people and take less than 30 minutes. It should be done in a Mediterranean style, and of course, it should be easy to make as I'm not very good at cooking"
+#sentence2 = "I would like to make a dessert for my daughter's birthday"
+# sentence3 = "I would like to make burritos with cream cheese filling and salsa, it should feed my whole family" 
+# manipulation = "How Many Servings? Carrots"
+#print(SlotFiller().get_ipi_prompt_information(sentence2))
